c51956ff73ce2f2bcb0a9cd1d71ed28c67a756b7
Danielle Loyola <danielle.loyola@developersinstitute.co.nz>
2022-07-19 22:10:30 +1200

    added game over function if player is hit with enemy bullet

--- index.js
+++ index.js
@@ -21,12 +21,40 @@ const enemyController = new EnemyController(
 );
 const player = new Player(canvas, 3, playerBulletController);
 
+let isGameOver = false;
+let didWin = false;
+
 function game() {
+  checkGameOver();
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  enemyController.draw(ctx);
-  player.draw(ctx);
-  playerBulletController.draw(ctx);
-  enemyBulletController.draw(ctx);
+  displayGameOver();
+  if (!isGameOver) {
+    enemyController.draw(ctx);
+    player.draw(ctx);
+    playerBulletController.draw(ctx);
+    enemyBulletController.draw(ctx);
+  }
+}
+
+function displayGameOver() {
+  if (isGameOver) {
+    let text = didWin ? "You Win" : "Game Over";
+    let textOffset = didWin ? 3.5 : 5;
+
+    ctx.fillStyle = "red";
+    ctx.font = "70px Arial";
+    ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);
+  }
+}
+
+function checkGameOver() {
+  if (isGameOver) {
+    return;
+  }
+
+  if (enemyBulletController.collideWith(player)) {
+    isGameOver = true;
+  }
 }
 
 setInterval(game, 1000 / 60);
